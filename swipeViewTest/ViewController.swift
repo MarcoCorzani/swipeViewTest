3b8f32ed03ec9be91058ccd1aa93eaf8e297f5ae
Marco <Marco@Marcos-iMac.fritz.box>
2015-03-02 21:36:08 +0100

    Initial Commit

---
+++ swipeViewTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swipeViewTest
+//
+//  Created by Marco F.A. Corzani on 02.03.15.
+//  Copyright (c) 2015 Alphaweb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
